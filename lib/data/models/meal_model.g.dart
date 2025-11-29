dab9ef0fc37f2ec5bcd2c521e5c2c36bbd9065d1
Ruxshona Baymanova <rbaymanova@gmail.com>
2025-11-29 09:56:37 +0500

    added firebase

---
+++ lib/data/models/meal_model.g.dart
@@ -0,0 +1,65 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'meal_model.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class MealModelAdapter extends TypeAdapter<MealModel> {
+  @override
+  final int typeId = 0;
+
+  @override
+  MealModel read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return MealModel(
+      id: fields[0] as String,
+      name: fields[1] as String,
+      category: fields[2] as String,
+      price: fields[3] as double,
+      imageUrl: fields[4] as String,
+      rating: fields[5] as double,
+      description: fields[6] as String,
+      ingredients: (fields[7] as List).cast<String>(),
+      area: fields[8] as String,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, MealModel obj) {
+    writer
+      ..writeByte(9)
+      ..writeByte(0)
+      ..write(obj.id)
+      ..writeByte(1)
+      ..write(obj.name)
+      ..writeByte(2)
+      ..write(obj.category)
+      ..writeByte(3)
+      ..write(obj.price)
+      ..writeByte(4)
+      ..write(obj.imageUrl)
+      ..writeByte(5)
+      ..write(obj.rating)
+      ..writeByte(6)
+      ..write(obj.description)
+      ..writeByte(7)
+      ..write(obj.ingredients)
+      ..writeByte(8)
+      ..write(obj.area);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is MealModelAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
