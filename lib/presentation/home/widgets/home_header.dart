7991deacd336da65d4dd6760f41ad41afe7170ce
Ruxshona Baymanova <rbaymanova@gmail.com>
2025-11-29 18:58:42 +0500

    shows user's name in home header

--- lib/presentation/home/widgets/home_header.dart
+++ lib/presentation/home/widgets/home_header.dart
@@ -1,8 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:xurmo/core/constants/app_text_styles.dart';
 
-class HomeHeader extends StatelessWidget {
+class HomeHeader extends StatefulWidget {
   const HomeHeader({super.key});
+
+  @override
+  State<HomeHeader> createState() => _HomeHeaderState();
+}
+
+class _HomeHeaderState extends State<HomeHeader> {
+  String _userName = 'User';
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchUserName();
+  }
+
+  Future<void> _fetchUserName() async {
+    final user = FirebaseAuth.instance.currentUser;
+
+    if (user != null) {
+      try {
+        final doc = await FirebaseFirestore.instance
+            .collection('users')
+            .doc(user.uid)
+            .get();
+
+        if (doc.exists && mounted) {
+          final data = doc.data();
+          setState(() {
+            // _userName = data?['name'] ?? 'User';
+
+            // Optional: If you want to show only the first name
+            _userName = (data?['name'] ?? 'User').split(' ')[0];
+          });
+        }
+      } catch (e) {
+        debugPrint("Error fetching name: $e");
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,7 +52,7 @@ class HomeHeader extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Text(
-            'Hello, User',
+            'Hello, $_userName',
             style: AppTextStyles.heading1,
           ),
           const SizedBox(height: 4),
